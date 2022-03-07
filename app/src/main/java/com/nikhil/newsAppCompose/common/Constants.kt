f7abc8fc6053e1c3e64013d077e2ee359ffb2ac4
qburst <nikhil.k@qburst.com>
2022-03-07 09:07:39 +0530

    Initial commit

---
+++ app/src/main/java/com/nikhil/newsAppCompose/common/Constants.kt
@@ -0,0 +1,9 @@
+package com.nikhil.newsAppCompose.common
+
+object Constants {
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = ""
+    const val EVERYTHING = "everything"
+    const val TOP_HEADLINES = "top-headlines"
+    const val DUMMY_IMAGE = "https://th.thgim.com/news/national/other-states/b4hecr/article38265141.ece/ALTERNATES/FREE_660/Union-Home-Minister-Amit-Shah-in-Mizapur"
+}
